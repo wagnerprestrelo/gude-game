8b95565e4070621e1bdf20a9a22babc8ccceecce
Wagner Prestrelo <wagner_prestrelo@hotmail.com>
2022-04-09 21:28:46 -0300

    Atualização da checagem de login e email

--- inserir.php
+++ inserir.php
@@ -2,10 +2,10 @@
 $link = mysqli_connect("localhost", "root", "", "gude_game");
 //echo $_POST['login'];
 //$query = "SELECT login FROM usuarios WHERE login = '{$_POST['login']}'";
-$result = mysqli_query($link, "SELECT login FROM usuarios WHERE login = '{$_POST['login']}'");
+$result = mysqli_query($link, "SELECT login, email FROM usuarios WHERE login = '{$_POST['login']}' OR email = '{$_POST['email']}'");
 
 if ($result->num_rows > 0){
-    echo "<p> Usuário já cadastrado.</p>";
+    echo "<p>Usuário e/ou email já cadastrado(s).</p>";
 } else {
     $result = mysqli_query($link, "INSERT INTO usuarios (login, senha, email) VALUES ('{$_POST['login']}', '{$_POST['senha']}', '{$_POST['email']}')");
     header('Location: cadastro.html');

5abdab3a2c321a633376774b4da4b83b2e5a1946
Wagner Prestrelo <wagner_prestrelo@hotmail.com>
2022-04-09 21:25:29 -0300

    Checando se login e email já estão cadastrados

--- inserir.php
+++ inserir.php
@@ -1,10 +1,17 @@
 <?php
 $link = mysqli_connect("localhost", "root", "", "gude_game");
 //echo $_POST['login'];
-$query = "INSERT INTO usuarios (login, senha, email) VALUES ('{$_POST['login']}', '{$_POST['senha']}', '{$_POST['email']}')";
+//$query = "SELECT login FROM usuarios WHERE login = '{$_POST['login']}'";
+$result = mysqli_query($link, "SELECT login FROM usuarios WHERE login = '{$_POST['login']}'");
 
-$result = mysqli_query($link, $query);
-header('Location: cadastro.html');
+if ($result->num_rows > 0){
+    echo "<p> Usuário já cadastrado.</p>";
+} else {
+    $result = mysqli_query($link, "INSERT INTO usuarios (login, senha, email) VALUES ('{$_POST['login']}', '{$_POST['senha']}', '{$_POST['email']}')");
+    header('Location: cadastro.html');
+}
+
+
 //echo "<p> '{$_POST['login']}' cadastrado com sucesso.";
 /* while ($row = mysqli_fetch_row($result)) {
     echo "<p> $row[0] - $row[1] - $row[2] </p>";
